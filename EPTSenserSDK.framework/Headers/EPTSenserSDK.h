279493f17df7a6bfebd1827e308da9d3fe4842e9
代号IX <395814391@qq.com>
2019-06-05 17:48:38 +0800

    release 0.0.2

---
+++ EPTSenserSDK.framework/Headers/EPTSenserSDK.h
@@ -0,0 +1,20 @@
+//
+//  EPTSenserSDK.h
+//  EPTSenserSDK
+//
+//  Created by Owen on 2019/5/30.
+//  Copyright © 2019 eggplant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EPTSenserSDK.
+FOUNDATION_EXPORT double EPTSenserSDKVersionNumber;
+
+//! Project version string for EPTSenserSDK.
+FOUNDATION_EXPORT const unsigned char EPTSenserSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EPTSenserSDK/PublicHeader.h>
+
+
+#import <EPTSenserSDK/EggplantSenserApi.h>
---
+++ EPTSenserSDK.framework/Headers/EggplantSenserApi.h
@@ -0,0 +1,69 @@
+//
+//  EggplantSenserApi.h
+//  EggplantSenserApi
+//
+//  Created by Owen on 2019/5/28.
+//  Copyright © 2019 eggplant. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol EggplantSenserApiDelegate <NSObject>
+
+@optional
+
+
+/**
+ tv连接完成回调
+
+ @param success 连接成功
+ @param error 成功时为nil，失败时返回NSError
+ */
+- (void)eggplantSenserTvConected:(BOOL)success error:(NSError *)error;
+
+/**
+ tv断开连接
+ */
+- (void)eggplantSenserTvDisconnected;
+
+@end
+
+@interface EggplantSenserApi : NSObject
+
+/**
+ 获取SDK版本号
+
+ @return 版本号
+ */
++ (NSString *)getApiVersion;
+
+/**
+ 停止采集数据
+ */
++ (void)stopDance;
+
+/**
+ 开始采集数据
+ */
++ (void)startDance;
+
+/**
+ 连接电视
+
+ @param ipString url 格式为"ws://192.168.2.38"
+ @param delegate EggplantSenserApiDelegate对象，用来接收SDK触发的消息。
+ */
++ (void)connectTvWithIpString:(NSString *)ipString delegate:(id<EggplantSenserApiDelegate>)delegate;
+
+/**
+ 断开电视连接
+ */
++ (void)disconnectTv;
+
+/** 设置是否在console输出sdk的log信息.
+ 
+ @param enable 默认NO(不输出log); 设置为YES, 输出可供调试参考的log信息. 发布产品时必须设置为NO.
+ */
++ (void)setLogEnabled:(BOOL)enable;
+
+@end
